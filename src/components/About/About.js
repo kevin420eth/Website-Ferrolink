1b23d17c1041736775deffef9060eaddf313907b
Kevin Mars <kevin.eth.420@gmail.com>
2023-03-09 06:16:38 +0800

    About section completed

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,35 @@ import './about.css'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className='section about__section'>
+      <h1>About</h1>
+      <div className='about__container'>
+        <p className='about__content'>
+          Ferrolink Industries co., Ltd. was established in 1997. In the early stage, the company is
+          specialized in various hot forging tools and manufacturing auto parts with accumulated more than
+          50 years experience. The company has been devoted to perfect, practical and competitve products
+          with continuous and active development and research. Our company is aimed at selling products in
+          the Europe and the US market. Currently, our product quality is comparable to the required level
+          of the advanced countries in Europe and America. Therefore, the customers can be more confident
+          and reassuring in marketing, and creat a niche for sustainable business.
+        </p>
+      </div>
+      <h1>Our Persitence</h1>
+      <div className='about__container'>
+        <p className='about__content'>
+          We always uphold the business philosophy and operating principles of “Diligence and Integrity”, 
+          “International Quality”, “Technical Innovation”, “Just-in-time Delievery” and “Customer 
+          Satisfaction”. Also, we insist on the highest quality standards to all our products and 
+          Integrity as the foundation of the company's brand and image. With presistence and hard work, 
+          our practical and competitve products designed by our stong and professional R&D team have gained 
+          the trust of the industry and customers and have earned good reupation and model among the hand 
+          tools and horticulture industry. In the future, our company will continue to enhance our 
+          professional stength and competitveness. By creating integration service and linking 
+          manufacturing, services, content and access providers, the company can create virtual and 
+          integrated services for the goal of sustainable business.
+        </p>
+      </div>
+    </section>
   )
 }
 
