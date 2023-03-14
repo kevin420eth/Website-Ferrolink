923cb62d391bc4f688e24842180a1bb9469fd984
Kevin Mars <kevin.eth.420@gmail.com>
2023-03-14 08:35:37 +0800

    Footer completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header/Header'
 import Home from './components/Home/Home'
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
         <Contact />
       </main>
       <footer>
-
+        <Footer />
       </footer>
     </div>
   );
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import './footer.css'
 
+
+
 const Footer = () => {
+  const year = new Date().getFullYear()
+
   return (
-    <div>Footer</div>
+    <section id='footer' className='section footer__section'>
+      <div className='footer__link'>
+        <a className='footer__link__text' href='#top'>Home</a>
+        <a className='footer__link__text' href='#product'>Product</a>
+        <a className='footer__link__text' href='#about'>About</a>
+        <a className='footer__link__text' href='#contact'>Contact</a>
+      </div>
+      <p className='footer__copyright__text'>Copyright © {year} Ferrolink Industries Co. All rights reserved.</p>
+    </section>
   )
 }
 
