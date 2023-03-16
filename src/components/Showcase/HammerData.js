de44614b7c71e0029049b065cf11a195e1135fa3
Kevin Mars <kevin.eth.420@gmail.com>
2023-03-16 09:45:15 +0800

    Product page building...

---
+++ src/components/Showcase/HammerData.js
@@ -0,0 +1,21 @@
+import Image1 from '../../assets/F-28.jpg'
+
+const HammerData = [
+    {
+        id: 1,
+        name: 'F-28',
+        image: Image1
+    },
+    {
+        id: 2,
+        name: 'F-28',
+        image: Image1
+    },
+    {
+        id: 3,
+        name: 'F-28',
+        image: Image1
+    },
+]
+
+export default HammerData;
--- src/components/Showcase/Showcase.js
+++ src/components/Showcase/Showcase.js
@@ -1,25 +1,29 @@
 import React from 'react'
 import './showcase.css'
+import HammerData from './HammerData'
 
 const Showcase = () => {
     return (
-        <section className='section showcase__section'>
-            <div className='showcase__container'>
+        <section className='section product__showcase__section'>
+            <div className='product__showcase__container'>
                 <div className='product__category__container'>
-                    <a href='#' className='product__category__link'>Hammers</a>
-                    <a href='#' className='product__category__link'>Axes & Hatchets</a>
-                    <a href='#' className='product__category__link'>Gardening Tools</a>
-                    <a href='#' className='product__category__link'>Handles</a>
-                    <a href='#' className='product__category__link'>Other Tools</a>
+                    <a href='/' className='product__category__link'>Hammers</a>
+                    <a href='#e' className='product__category__link'>Axes & Hatchets</a>
+                    <a href='#e' className='product__category__link'>Gardening Tools</a>
+                    <a href='#e' className='product__category__link'>Handles</a>
+                    <a href='#e' className='product__category__link'>Other Tools</a>
                 </div>
 
-                <div className='product__showcase'>
-                    <div className='product__showcase__container'>
-                        <div className='product__preview'>fsddfff</div>
-                        <div className='product__preview'>fff</div>
-                        <div className='product__preview'>fff</div>
-                        <div className='product__preview'>fff</div>
-                        <div className='product__preview'>fff</div>
+                <div className='product__preview__section'>
+                    <div className='product__preview__container'>
+                        {HammerData.map((item) => {
+                            return (
+                                <div className='product__preview' key={item.id}>
+                                    <img className='product__preview__image' src={item.image} alt='img' />
+                                    <p className='product__preview__name'>{item.name}</p>
+                                </div>
+                            )
+                        })}
                     </div>
                 </div>
             </div>
