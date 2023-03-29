6e00e925f66e820225a0d8036ca7a90a392bf542
Kevin Mars <kevin.eth.420@gmail.com>
2023-03-29 13:51:55 +0800

    single product page completed

--- src/components/SingleProduct/SingleProduct.js
+++ src/components/SingleProduct/SingleProduct.js
@@ -10,13 +10,30 @@ const SingleProduct = () => {
     return (
         <div>
             <Header />
-            <section className='section'>
+            <section className='single__product__section section'>
                 {Data.map((item) => {
                     if (item.category === productType && item.name === productName) {
                         return (
-                            <div>
-                                <p>{item.name}</p>
-                                <img src={item.image} alt={item.img} />
+                            <div className='single__product__container'>
+                                <ul className="single__product__info">
+                                    <li className='single__product__label'>Item Name</li>
+                                    <li className="single__product__content">Super huge jumbo hammer{}</li>
+                                    <li className="single__product__label">Number</li>
+                                    <li className="single__product__content">{item.name}</li>
+                                    <li className='single__product__label'>Size</li>
+                                    <li className="single__product__content">50x100x50 inches{}</li>
+                                    <li className="single__product__label">Weight</li>
+                                    <li className="single__product__content">60oz{}</li>
+                                    <li className='single__product__label'>Handle Material</li>
+                                    <li className="single__product__content">Alloy Steel{}</li>
+                                    <li className="single__product__label">Grip Type</li>
+                                    <li className="single__product__content">Rubber{}</li>
+                                    <li className="single__product__label">About</li>
+                                    <li className="single__product__content">This is a hammer</li>
+                                </ul>
+                                <div className="single__product__showcase">
+                                    <img src={item.image} alt={item.img} />
+                                </div>
                             </div>
                         )
                     }
