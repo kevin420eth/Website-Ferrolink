fbda2f74e174a7fe78f9fadf1a1a706a38a2df11
Kevin Mars <kevin.eth.420@gmail.com>
2023-04-09 17:33:49 +0800

    service section completed

--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './service.css'
+import icon1 from '../../assets/materials/icon1.png'
+import icon2 from '../../assets/materials/icon2.png'
+import icon3 from '../../assets/materials/icon3.png'
 
 const service = () => {
   return (
@@ -12,6 +15,9 @@ const service = () => {
           <div className="service__block__content">
             <p className='service__block__content__text'>Customize hand tools for your specific needs, creating the perfect tools you want with our experienced engineer.</p>
           </div>
+          <div className='image__container'>
+            <img class='icon__image' src={icon1} alt='icon_1'/>
+          </div>
         </div>
 
         <div class="service__block">
@@ -19,13 +25,18 @@ const service = () => {
           <div className="service__block__content">
             <p className='service__block__content__text'>Produce every part of tools that meets the highest standards of quality and accuracy with precision machining.</p>
           </div>
-
+          <div className='image__container'>
+            <img class='icon__image' src={icon2} alt='icon_1'/>
+          </div>
         </div>
 
         <div class="service__block">
           <h3 className='service__block__title'>Forging Engineering</h3>
           <div className="service__block__content">
             <p className='service__block__content__text'>Shape the tools with high strength and durability. We work with a variety of metals, including carbon steel, alloy steel, stainless steel, aluminum and brass.</p>
+          </div>
+          <div className='image__container'>
+            <img class='icon__image' src={icon3} alt='icon_1'/>
           </div>
         </div>
 
